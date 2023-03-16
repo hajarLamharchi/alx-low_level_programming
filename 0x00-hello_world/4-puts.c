b21d699d71345c9b8b00ae23a521587bcf936797
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-16 21:46:06 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/*
- * puts
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
