70fc03ae82ab9860bdcbef8917f2efbad7f6fd0d
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-16 22:27:05 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-	printf("char is %2d bytes\n", sizeof(char));
-	printf("int is %2d bytes\n", sizeof(int));
-	printf("float is %2d bytes\n", sizeof(float));
-	printf("long is %2d bytes\n", sizeof(long));
-	printf("unsigned char is %2d bytes\n", sizeof(unsigned char));
-	printf("unsigned int is %2d bytes\n", sizeof(unsigned int));
-	printf("unsigned float is %2d bytes\n", sizeof(unsigned float));
-	printf("unsigned long is %2d bytes\n", sizeof(unsigned long));
+	printf("char is %ld bytes\n", sizeof(char));
+	printf("int is %ld bytes\n", sizeof(int));
+	printf("float is %ld bytes\n", sizeof(float));
+	printf("long is %ld bytes\n", sizeof(long));
+	printf("unsigned char is %ld bytes\n", sizeof(unsigned char));
+	printf("unsigned int is %ld bytes\n", sizeof(unsigned int));
+	printf("unsigned float is %ld bytes\n", sizeof(unsigned float));
+	printf("unsigned long is %ld bytes\n", sizeof(unsigned long));
 	return (0);
 }
