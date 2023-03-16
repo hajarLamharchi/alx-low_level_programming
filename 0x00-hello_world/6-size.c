b7c1806f9411c5f0744486d6dbaeeaa1b7342477
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-16 22:34:05 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-	printf("char is %zu bytes\n", sizeof(char));
-	printf("int is %zu bytes\n", sizeof(int));
-	printf("float is %zu bytes\n", sizeof(float));
-	printf("double is %zu bytes\n", sizeof(double));
+	printf("size of char: %zu byte(s)\n", sizeof(char));
+	printf("size of int: %zu byte(s)\n", sizeof(int));
+	printf("size of float: %zu byte(s)\n", sizeof(float));
+	printf("size of double: %zu byte(s)\n", sizeof(double));
 	return (0);
 }
