e9d127c35196a8c00ac5fdb0a77bf035af9f3c6f
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-16 23:41:36 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@ int main(void)
 	char s1[100] = "and that piece of art is useful\" ";
 	char s2[100] = "- Dora Korpar, 2015-10-19";
 
-	fprintf(stderr, s1 + s2\n);
+	fprintf(stderr, s1 + s2);
 	return (1);
 }
