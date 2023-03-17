1a2f2c26b6a9316f634223a64f088ed9012f61b1
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-17 14:31:48 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c1 = 'a';
+	char c2 = 'A';
+
+	while (c1 <= 122)
+	{
+		putchar(c1);
+		c1++;
+
+	while (c2 <= 90)
+	{
+		putchar(c2);
+		c2++;
+	}
+	putchar('\n');
+	return (0);
+}
