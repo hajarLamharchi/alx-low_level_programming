a008ce2e369bc16ff7a9ae1cf323196c3e3e2d39
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-17 15:43:21 +0000

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
