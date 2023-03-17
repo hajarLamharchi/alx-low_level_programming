d3d5ec9728f25f046d9d662a20755c5956cd5c2a
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-17 17:08:37 +0000

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j = i + 1;
+
+		while (j < 10)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
