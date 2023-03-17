1c5415b3be4c821a494e84ca2b7adbfe3312b0af
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-17 17:38:43 +0000

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,25 +9,25 @@ int main(void)
 {
 	int i = 0;
 
-	while (i < 10)
+	while (i < 9)
 	{
 		int j = i + 1;
 
 		while (j < 10)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 9 && j != 9)
+			if (i != 8)
 			{
+				putchar(i + '0');
+				putchar(j + '0');
 				putchar(',');
 				putchar(' ');
+				j++;
 			}
 			else
 			{
-			j++;
+				i++;
 			}
 		}
-		i++;
 	}
 	putchar('\n');
 	return (0);
