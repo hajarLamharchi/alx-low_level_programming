463334cc1f70716c966bda12134e1c6f61836fbe
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-17 18:26:56 +0000

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 7)
+	{
+		int j = i + 1;
+
+		while (j <= 8)
+		{
+			int k = j + 1;
+
+			while (k <= 9)
+			{
+			putchar(i + '0');
+		putchar(j + '0');
+	putchar(k + '0');
+	if (i != 7)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
