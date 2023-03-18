1591c2c3e62888f3efdc4e9bb77770d79f2b12ac
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-18 19:55:39 +0000

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 8 ; j++)
+		{
+			for (k = i ; k <= 9 ; k++)
+			{
+				for (l = j + 1 ; l <= 9 ; l++)
+				{
+					if (i != 9 && j != 8)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(l + '0');
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
