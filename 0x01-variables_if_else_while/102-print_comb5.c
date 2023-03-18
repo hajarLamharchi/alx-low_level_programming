be1c7715e1bedc6c5278e98760967a331771558b
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-18 21:39:58 +0000

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,31 +7,19 @@
 
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0 ; i <= 98 ; i++)
 	{
-		for (j = 0 ; j <= 8 ; j++)
+		for (j = i + 1 ; j <= 99 ; j++)
 		{
-			for (k = i ; k <= 9 ; k++)
+			if (i != 98 && j != 99)
 			{
-				l = 0;
-				while (l <= 9)
-				{
-					if (l == 0 && k == 0)
-						continue;
-					else if (i != 9 && j != 8)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(l + '0');
-						putchar(',');
-						putchar(' ');
-					}
-					l++;
-				}
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
 			}
 		}
 	}
