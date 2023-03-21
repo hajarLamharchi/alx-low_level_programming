c273d0c35a05ac1210cb50e01dcb2baa6fcb447d
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-21 22:40:16 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(t / 10 + '0');
 				_putchar(t % 10 + '0');
 			}
-			else if (t >= 10 && j == o)
+			else if (t >= 10 && j == 0)
 			{
 			_putchar(t / 10 + '0');
 			_putchar(t % 10 + '0');
