e205af7cac676ab17543af4848efc76445266e32
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-21 22:50:03 +0000

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - return the addition of two integer
+ *
+ * @a: function parameter
+ * @b: function parameter
+ *
+ * Return: the addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
