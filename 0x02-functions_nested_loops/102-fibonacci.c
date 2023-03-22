35d0ead9b0cbd3c1c666d9310a9808ff996fde3d
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-22 14:25:26 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print the fibonacci numbers
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int i = 2;
+	int t[50];
+
+	t[0] = 1;
+	t[1] = 2;
+	printf("1, 2, ");
+	while (i < 50)
+	{
+		t[i] = t[i - 1] + t[i - 2];
+		printf("%d, ", t[i]);
+	}
+	return (0);
+}
+
