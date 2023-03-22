c37e7a6075e619cb16cd2e6b734adc5809052cbe
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-22 14:06:26 +0000

    sum of naturals

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: the sum f natural numbers multiples of 3 and 5
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	return (sum);
+}
+
