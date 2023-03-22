bd7039aaeb899dc2b47a2f3ffe180f1a6c662479
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-22 15:19:47 +0000

    fibonacci2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - sum of even valued terms in febunacci
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int i = 2;
+	unsigned long sum = 1;
+	unsigned long t[];
+
+	t[0] = 1;
+	t[1] = 2;
+	while(t[i] < 4000000)
+	{
+		t[i] = t[i - 1] + t[i - 2];
+		if (i % 2 == 0)
+			sum = sum + t[i];
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
