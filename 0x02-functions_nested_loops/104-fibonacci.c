08ef594fe19540f8d6f4b77649134d9df54b2612
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-22 16:09:01 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print the first 98 fibonacci numbers
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int count = 3;
+	unsigned long x = 1, y = 2, z = 0;
+
+	printf("1, 2, ");
+	while (count < 98)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		count++;
+		printf("%lu, ", z);
+	}
+	printf("%lu\n", x + y);
+	return (0);
+}
