6f9fb4736023106a28ed4a4a655f48871f3de649
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-22 14:40:07 +0000

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,11 +15,15 @@ int main(void)
 	printf("1, 2, ");
 	while (i < 50)
 	{
+		if (i != 49)
+		{
 		t[i] = t[i - 1] + t[i - 2];
 		printf("%lu, ", t[i]);
 		i++;
+		}
+		else
+			printf("%lu\n", t[i]);
 	}
-	printf("\n");
 	return (0);
 }
 
