aeaa4f8240f09baad6188adf55ca3a45896c2fb4
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-23 02:39:07 +0000

    positive negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,31 +1,18 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include "main.h"
 
-/* more headers goes there*/
-
-/*betty style doc for function main goes there*/
 /**
- * main - Entry point
+ * positive_or_negative - checks if an integer is positive negative or zero
+ * @i: function parameter the integer to check
  *
  * Return: Always 0 (success)
  */
-
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else if (i > 0)
+		printf("%d is positive\n", i);
 	else
-		printf("%d is zero\n", n);
-	return (0);
+		printf("%d is zero\n", i);
 }
