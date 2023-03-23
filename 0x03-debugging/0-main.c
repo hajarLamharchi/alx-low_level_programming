4ee90aad87e686992c260a2c2496248423177c38
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-23 01:47:19 +0000

    positive negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_ornegative(i);
+	printf("%d is zero", i);
+	return (0);
+}
