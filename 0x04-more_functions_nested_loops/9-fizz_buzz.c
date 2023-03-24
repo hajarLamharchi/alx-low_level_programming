e74f56f48123b84626aa38ab8dde0076af56686c
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-24 00:18:17 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,17 @@ int main(void)
 	printf("1");
 	for (i = 2; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" Fizz");
+			printf(" FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf(" Buzz");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 3 == 0)
 		{
-			printf(" FizzBuzz");
+			printf(" Fizz");
 		}
 		else
 		{
