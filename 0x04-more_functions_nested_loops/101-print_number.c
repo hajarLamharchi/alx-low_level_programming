c7264ca6cf3522d8160a625a2b881372d056f755
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-24 14:35:52 +0000

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,32 +8,12 @@
 
 void print_number(int n)
 {
-	if (n >= 1000)
-	{
-	_putchar(n / 1000 + '0');
-	_putchar(n / 100 % 10 + '0');
-	_putchar(n / 10 % 10 + '0');
-	_putchar(n % 10 + '0');
-	}
-	else if (n >= 100)
-	{
-	_putchar(n / 100 % 10 + '0');
-        _putchar(n / 10 % 10 + '0');
-        _putchar(n % 10 + '0');
-	}
-	else if (n >= 10)
-	{
-	_putchar(n / 10 % 10 + '0');
-        _putchar(n % 10 + '0');
-	}
-	else if (n == 0)
-	{
-	_putchar(n);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
+		n = -n;
 	}
+	if (n > 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
