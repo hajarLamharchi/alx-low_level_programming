1fa30c768a3449dab24f9923267203de63bad2df
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-27 23:43:15 +0000

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,4 +6,19 @@
  */
 void rev_string(char *s)
 {
+	int len = 0;
+	int i, j = 0;
+	char temp;
 
+	while (s[j] != '\0')
+	{
+		len++;
+		j++;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
+	}
+}
