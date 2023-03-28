a718fd6d2132027b060f182206b16028800fb604
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-28 01:19:40 +0000

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,6 +7,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+	int len = 0, i;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len: i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[len] = '\0';
 	return (dest);
 }
