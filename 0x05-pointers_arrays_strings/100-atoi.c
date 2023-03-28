9246e5cc4e811ae2febffd0437e44d4e54f1f834
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-28 15:52:37 +0000

    convert

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,3 +6,33 @@
  */
 int _atoi(char *s)
 {
+	int i = 0;
+	int len = 0;
+	int j;
+	int k;
+	int d = 0;
+	unsigned int n = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	for (j = 0; j < len ; j++)
+	{
+		if (s[j] >= '0' && s[j] <= '9')
+		{
+			n = n * 10 + s[j];
+		}
+	}
+	for (k = 0; k < len; k++)
+	{
+		if (s[k] == '-')
+			d++;
+	}
+	if (d % 2 != 0)
+	{
+		n = -n;
+	}
+	return (n);
+}
