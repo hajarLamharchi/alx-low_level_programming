d10623a4eea71164265d90678cfc8fd6cd070518
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-29 23:44:59 +0000

    compare

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _atoi - convert a string into an integer
  * @s: the string to convert
@@ -7,27 +8,38 @@
 int _atoi(char *s)
 {
 	int index = 0;
-	int i = 0, j, k, d = 0;
-	unsigned int n = 0;
-
-	while (s[i] < 48 && s[i] > 57)
+	int i, j, k, d = 0;
+	float n = 0;
+	int ord;
+	int len;
+	int digit;
+	
+	while (s[len] != '\0')
+		len++;
+	for (i = 0; i < len; i++)
 	{
-		index++;
-		i++;
+		index = i;
+		ord = s[i];
+		if (ord >= 48  && ord <= 57)
+			break;
 	}
-	for (j = 0; j < index; j++)
+	for (j = 0; j <= index; j++)
 	{
 		if (s[j] == '-')
+		{
 			d++;
+		}
 	}
-	k = index;
-	while (s[k] >= 48 && s[k] <= 57)
+	for (k = index; k < len; k++)
 	{
-		n = n * 10 + s[k];
+		digit = s[k] - '0';
+		ord = s[k];
+	if (ord < 48 && ord > 57)
+	break;
+	else
+	n = n * 10 + digit;
 	}
 	if (d % 2 != 0)
-	{
 		n = -n;
-	}
 	return (n);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,24 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == s2[i])
 		{
-			return (s1[i] - s2[i]);
+			i++;
 		}
 		else
 		{
-			return (0);
+			break;
 		}
+	}
+	if (s2[i] == '\0')
+	{
+		return (0);
+	}
+	if (s1[i] != s2[i])
+	{
+		return (s1[i] - s2[i]);
 	}
 	return (0);
 }
