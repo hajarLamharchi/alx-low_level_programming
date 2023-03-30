26d22683ce550871b5fd6e8d8d69021dcf0c6c7c
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-03-30 16:05:13 +0000

    fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,22 +7,16 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char *ch1 = "abcdefghijklm";
-	char *ch2 = "ABCDEFGHIJKLM";
-	char *ch3 = "nopqrstuvwxyz";
-	char *ch4 = "NOPQRSTUVWXYZ";
+	char *ch1 = "abcdefghijklmABCDEFGHIJKLMnopqrstuvwxyzNOPQRSTUVWXYZ";
+	char *ch2 = "nopqrstuvwxyzNOPQRSTUVWXYZabcdefghijklmABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 13; j++)
+		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == ch1[j] || s[i] == ch2[j])
+			if (s[i] == ch1[j])
 			{
-				s[i] += 13;
-			}
-			else if (s[i] == ch3[j] || s[i] == ch4[j])
-			{
-				s[i] -= 13;
+				s[i] = ch2[j];
 			}
 		}
 	}
