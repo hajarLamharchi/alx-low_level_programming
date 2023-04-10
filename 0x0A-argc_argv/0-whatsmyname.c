c4533481d9ec0512913fe9fc05f8e957640b7879
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-10 15:21:57 +0000

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: Always 0
+ */
+int main(int __attribute__ ((unused)) argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
