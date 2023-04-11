aafdcb73238ad5f69d8f0a595728cc7e739b44b4
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-11 17:01:05 +0000

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,16 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ptr = malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
 	for (i = 0; i < size; i++)
 	{
 		*(ptr + i) = c;
+	}
 	}
 	return (ptr);
 }
