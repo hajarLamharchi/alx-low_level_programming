840f3ce0e625478fe50b3b6c7e501f10cf3e8794
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-11 23:13:19 +0000

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		*s1 = ' ';
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		*s2 = ' ';
+		s2 = "";
 	}
 	size1 = strlen(s1);
 	size2 = strlen(s2);
