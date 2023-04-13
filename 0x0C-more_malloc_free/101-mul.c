f1f98e7c90cff1454368af78c289cd531742152c
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-13 23:53:18 +0000

    malloc free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * main - calculate the multiplication of two numbers
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long int *mul, num1, num2;
+	unsigned long int len, len1, len2;
+	int i, j;
+
+	num1 = *argv[1];
+	num2 = *argv[2];
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; *argv[j] != '\0'; j++)
+		{
+			if (!(isdigit(*argv[j])))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	len1 = strlen((char *)num1);
+	len2 = strlen((char *)num2);
+	len = len1 + len2 + 1;
+	mul = malloc(sizeof(int) * len);
+	*mul = num1 * num2;
+	printf("%lu\n", *mul);
+	return (0);
+}
