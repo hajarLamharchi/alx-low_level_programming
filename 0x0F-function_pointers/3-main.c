3b290bffe6693ab9b185f27997650d69eb82f944
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-19 20:52:16 +0000

    task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,16 +23,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (*operator != '+' &&
-			*operator != '-' &&
-			*operator != '*' &&
-			*operator != '/' &&
-			*operator != '%')
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*operator == '/' || *operator == '%') && num2 == '0')
+	if ((*operator == '/' && num2 == 0) ||
+			(*operator == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
