4bc40e96c50b274309930714df7f41682bdecc01
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-20 17:18:42 +0000

    hot fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,15 +11,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	char *str;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(ap, char *) == NULL)
+		str = va_arg(ap, char *);
+		if (str == NULL)
 		{
 			printf("(nil)");
 		}
-		printf("%s", va_arg(ap, char *));
+		else
+		{
+			printf("%s", str);
+		}
 		if (separator != NULL && i != (n - 1))
 		{
 			printf("%s ", separator);
