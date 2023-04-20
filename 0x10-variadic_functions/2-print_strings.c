d8f1b68acdae77fe6fe99167c452b72297677b5b
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-20 17:36:51 +0000

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		if (separator != NULL && i != (n - 1))
 		{
-			printf("%s ", separator);
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
