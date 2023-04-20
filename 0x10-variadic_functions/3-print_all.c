68dc58dbbeb7dd68ffb02784c8359d0ca2887a61
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-20 21:29:28 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,42 +8,37 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	char *separator = "";
 	int i = 0;
 	char *str;
 
 	va_start(ap, format);
-	if (format)
+	while (format && format[i])
 	{
-		while (format[i])
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%s%c", separator, va_arg(ap, int));
-					break;
-				case 'i':
-					printf("%s%d", separator, va_arg(ap, int));
-					break;
-				case 'f':
-					printf("%s%f", separator, va_arg(ap, double));
-					break;
-				case 's':
-					str = va_arg(ap, char *);
-					if (!str)
-					{
-						str = "(nil)";
-					}
-					printf("%s%s", separator, str);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			separator = ",";
-			i++;
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (!str)
+				{
+					str = "(nil)";
+				}
+				printf("%s", str);
+				break;
 		}
-	}
+		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i'
+					|| format[i] == 'f' || format[i] == 's'))
+			printf(",");
+		i++;
+		}
 	printf("\n");
 	va_end(ap);
 }
