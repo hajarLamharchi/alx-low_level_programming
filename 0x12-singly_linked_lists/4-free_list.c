59f2fff2b51eced8e75a7342873a74eab0f18fb5
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-27 16:39:27 +0000

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - free a linked list
+ * @head: the pointer to the head of the list
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current_node = head;
+	list_t *next_node;
+
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
+	}
+}
