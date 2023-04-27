a4311b45191fe7ffd66c5f249c5a4ff5fc137420
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-27 17:01:27 +0000

    task5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_before - prints a line before main function i executed
+ * Return: nothing
+ */
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
