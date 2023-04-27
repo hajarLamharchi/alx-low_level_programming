77cfa3feb561f546c6a2155a431dc63a6885dc8f
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-27 15:45:20 +0000

    task0

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add new element at the end of a linked list
+ * @head: a pointer to a pointer to the head of the list
+ * @str: the element to add
+ * Return: address to the new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
