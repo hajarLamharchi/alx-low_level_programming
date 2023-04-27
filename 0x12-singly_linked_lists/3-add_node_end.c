96105215270605c0152db6a3eb8ccc81494c84a0
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-27 16:29:18 +0000

    task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,3 +9,28 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *temp = malloc(sizeof(list_t));
+	list_t *current = *head;
+
+	if (temp == NULL)
+		return (NULL);
+
+	temp->str = strdup(str);
+	temp->len = strlen(str);
+
+	if (*head == NULL)
+	{
+		*head = temp;
+	}
+	else
+	{
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = temp;
+		temp->next = NULL;
+	}
+
+	return (temp);
+}
