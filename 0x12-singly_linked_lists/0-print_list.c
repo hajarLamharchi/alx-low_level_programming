bacce60f5d9ee36e3f6a9f60c240209a41a1a8f2
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-27 14:55:27 +0000

    fix1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,22 +7,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	list_t *ptr = NULL;
-	unsigned int count = 0;
+	size_t count = 0;
 
-	ptr = (list_t *)h;
-
-	while (ptr != NULL)
+	while (h != NULL)
 	{
-		if (ptr->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%u] (%s)\n", ptr->len, ptr->str);
+			printf("[%u] (%s)\n", h->len, h->str);
 		}
-		ptr = ptr->next;
+		h = h->next;
 		count++;
 	}
 
