f75ab6f7abc8456511b2e5fa8fd376fc0a414eda
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-04-29 12:04:15 +0000

    task4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -11,7 +11,7 @@ void free_listint(listint_t *head)
 	listint_t *current_node = head;
 	listint_t *next_node;
 
-	while (current_node->next != NULL)
+	while (current_node != NULL)
 	{
 		next_node = current_node->next;
 		free(current_node);
