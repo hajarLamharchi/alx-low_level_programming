0719c515f6d21742577119b1e8a1a45f4829c16a
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-05-08 14:58:16 +0000

    task2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: file input
+ * @text_content: the content to append
+ * Return: 1 on success -1 otherwise
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, w, len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	len = strlen(text_content);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	w = write(fd, text_content, len);
+	if (fd == -1 || w == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
