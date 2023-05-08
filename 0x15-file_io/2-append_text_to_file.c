297df1fe7746b6d65d7c994af1cd7b79060cd9d4
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-05-08 17:32:01 +0000

    fix task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,13 +15,26 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	len = strlen(text_content);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	w = write(fd, text_content, len);
-	if (fd == -1 || w == -1)
+	if (text_content == NULL)
 	{
 		return (-1);
 	}
+	len = strlen(text_content);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	w = write(fd, text_content, len);
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 	close(fd);
+	if (close(fd) == -1)
+	{
+		return (-1);
+	}
 	return (1);
 }
