7070a7f1da4409baba4fea40f1343b519580f3e1
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-07-07 10:39:04 +0000

    fix task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *tab;
 
-	tab = (hash_table_t *)malloc(sizeof(hash_table_t));
+	tab = malloc(sizeof(hash_table_t));
 	if (!tab)
 		return (NULL);
 
