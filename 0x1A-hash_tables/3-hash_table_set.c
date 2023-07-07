4b7c803db3a95ae35edc17157c760a8e935fd9b1
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-07-07 11:44:55 +0000

    fix task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (key == NULL || ht == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
