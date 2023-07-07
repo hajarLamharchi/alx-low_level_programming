b60362c89328ee0fd28af25de10f3e7541f4e20f
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-07-07 11:51:09 +0000

    fix task4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int i;
 	hash_node_t *node;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
 	i = key_index((const unsigned char *)key, ht->size);
+	if (i >= ht->size)
+		return (NULL);
+
 	node = ht->array[i];
 
 	while (node != NULL)
