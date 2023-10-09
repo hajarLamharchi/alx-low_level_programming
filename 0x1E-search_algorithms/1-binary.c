f3330c9912700c059325b1c1a3c78ee0edfaf168
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-10-09 21:51:21 +0000

    binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,26 +9,26 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t l = 0;
-	size_t r = size - 1;
+	size_t left = 0;
+	size_t right = size - 1;
 	size_t m, i;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	while (l <= r)
+	while (left <= right)
 	{
 		printf("Searching in array: ");
-		for (i = l; i < r; i++)
+		for (i = left; i < right; i++)
 		{
 			printf("%ld, ", i);
 		}
 		printf("%ld\n", i);
-		m = (l + r) / 2;
+		m = (left + right) / 2;
 		if (array[m] < value)
-			l = m + 1;
+			left = m + 1;
 		else if (array[m] > value)
-			r = m - 1;
+			right = m - 1;
 		else
 			return (m);
 	}
